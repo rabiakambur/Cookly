240e66eaf573c973bff03a6f964083b9f5ac352f
Rabia Kambur <genisrabia@gmail.com>
2024-06-05 21:28:17 +0300

    Implement dagger hilt

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.kapt) apply false
+    alias(libs.plugins.dagger) apply false
 }
