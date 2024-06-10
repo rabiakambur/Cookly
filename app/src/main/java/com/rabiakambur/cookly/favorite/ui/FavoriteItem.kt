4cf716b8f82368b66a946fe26fe88941fe8c08d9
Rabia Kambur <genisrabia@gmail.com>
2024-06-10 12:29:53 +0300

    Implement favorite deletion dialog

--- app/src/main/java/com/rabiakambur/cookly/favorite/ui/FavoriteItem.kt
+++ app/src/main/java/com/rabiakambur/cookly/favorite/ui/FavoriteItem.kt
@@ -1,19 +1,29 @@
 package com.rabiakambur.cookly.favorite.ui
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.rabiakambur.cookly.favorite.data.source.local.RecipeEntity
@@ -24,6 +34,8 @@ fun FavoriteItem(
     favoriteRecipe: RecipeEntity,
     onDeleteClick: (RecipeEntity) -> Unit
 ) {
+    var shouldShowItemDeletionDialog by remember { mutableStateOf(false) }
+
     Card(
         modifier = Modifier
             .padding(start = 10.dp, end = 10.dp, top = 10.dp, bottom = 5.dp)
@@ -45,12 +57,40 @@ fun FavoriteItem(
             modifier = Modifier
                 .padding(10.dp)
         ) {
-            Text(
-                text = "sdfsdfsdfsdfsdfsdfsdfsdflksflsdkşflsdkfşlksdşlfkşsdlkfşl",
-                textAlign = TextAlign.Justify,
-                fontSize = 13.sp,
-                color = Color.Black
-            )
+            Row {
+                Text(
+                    text = "sdfsdfsdfsdfsdfsdfsdfsdflksflsdkşflsdkfşlksdşlfkşsdlkfşl",
+                    textAlign = TextAlign.Justify,
+                    fontSize = 13.sp,
+                    color = Color.Black
+                )
+                Icon(
+                    tint = Color.Black,
+                    imageVector = Icons.Filled.Delete,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .clickable {
+                            shouldShowItemDeletionDialog = true
+                        }
+                )
+                if (shouldShowItemDeletionDialog) {
+                    FavoriteItemDeletionDialog({
+                        shouldShowItemDeletionDialog = it
+                    }, {
+                        onDeleteClick(favoriteRecipe)
+                    })
+                }
+            }
         }
     }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun FavoriteItemPreview() {
+    FavoriteItem(
+        favoriteRecipe = RecipeEntity(uid = 1, name = "", image = "", isFavorite = true),
+        onDeleteClick = {
+
+        })
 }
