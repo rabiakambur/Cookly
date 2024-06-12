bddf97d7816c6051d642eb359ba2ca93db3ac9c2
Rabia Kambur <genisrabia@gmail.com>
2024-06-12 18:11:09 +0300

    Implement error and loading icon

--- app/src/main/java/com/rabiakambur/cookly/home/ui/HomeScreen.kt
+++ app/src/main/java/com/rabiakambur/cookly/home/ui/HomeScreen.kt
@@ -1,11 +1,15 @@
 package com.rabiakambur.cookly.home.ui
 
 import androidx.activity.ComponentActivity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -16,8 +20,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.rabiakambur.cookly.R
 import com.rabiakambur.cookly.home.ui.component.HeaderComponent
 import com.rabiakambur.cookly.main.theme.BackgroundColor
 
@@ -52,20 +61,65 @@ fun HomeScreen(
                     }
                 }
             )
-
             if (state.isLoading) {
-                Text(
-                    modifier = Modifier.align(Alignment.Center),
-                    text = "Şuanda veriler çekiliyor."
-                )
+                Loading()
             }
 
             if (state.isError) {
+                Error()
+            }
+        }
+    }
+}
+@Composable
+fun Loading() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_loading_foreground),
+            contentDescription = stringResource(R.string.loading),
+            modifier = Modifier
+                .size(200.dp)
+        )
+    }
+}
+
+@Composable
+fun Error() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box (
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_error_foreground),
+                    contentDescription = stringResource(R.string.content_description_error),
+                    modifier = Modifier
+                        .size(300.dp)
+                        .align(Alignment.Center)
+                )
                 Text(
-                    modifier = Modifier.align(Alignment.Center),
-                    text = "Birader şuanda bir sıkıntı var, acele etme ;)."
+                    text = stringResource(R.string.error),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Medium,
+                    modifier = Modifier
+                        .padding(top = 2.dp, bottom = 40.dp)
+                        .align(Alignment.BottomCenter)
+
                 )
             }
+
         }
     }
 }
